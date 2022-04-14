ec5c670f4ecad40ca88743da19efa342b80d04a4
drewEacker <you@example.com>
2022-04-13 20:45:06 -0400

    mid session wedensdayt the 13th

---
+++ Session 17/script.js
@@ -0,0 +1,23 @@
+const submitButton = document.querySelector('#fetch')
+const breedInput = document.querySelector('input')
+const imageDiv = document.querySelector('div')
+
+
+const getBreeds = async() => {
+
+    const dogBreeds = await axios.get('https://dog.ceo/api/breeds/list/all')
+
+    console.log(dogBreeds)
+}
+
+getBreeds()
+
+
+submitButton.addEventListener('click', async () => {
+    let breed = breedInput.value
+    let response = await axios.get(`https://dog.ceo/api/breed/${breed}/images/random)`)
+console.log(response.data.message)
+let dogPic = response.data.message
+imageDiv.innerHTML = `<img src=${dogPic}>`
+
+})
