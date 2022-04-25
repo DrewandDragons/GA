28a1035e98e64f37a3a9fd87a9d47a2ab89df985
drewEacker <you@example.com>
2022-04-25 01:28:46 -0400

    doing a lot of CSS, Media Queries and workling on animations

--- Final Project - GA Week 10/script.js
+++ Final Project - GA Week 10/script.js
@@ -1,17 +1,3 @@
-// let partySize = document.querySelector
-
-let filler = document.getElementById('filler')
-let ribbon = document.querySelector(".classes")
-let pcCard = document.querySelector(".starting-stats")
-const classApi = `https://www.dnd5eapi.co/api/classes/`
-
-//Consts above 
-// async function get5E() {
-//     const response = await fetch(classApi);
-//     const data = await response.json();
-//     console.log(data)
-// }
-// get5E();
 
 async function getClasses(url) {
     
@@ -75,9 +61,9 @@ document.querySelectorAll('.classes').forEach(item => {
         const classesApi = `https://www.dnd5eapi.co/api/classes/${clazz}/`
         let classRibbon =  document.querySelector(`[data-character-class="${clazz}"]`)
         let ribbon = document.querySelector(`[data-character-class="${clazz}"] .starting-stats`)
-    
-        ribbon.style.display = 'none'
 
+        classRibbon.style.borderColor = 'black'  
+        ribbon.style.display = 'none'
     })
 
     //trying to get text to show when they get to 4fr size... though now I think that wont work on smaller screeen
@@ -97,13 +83,16 @@ document.querySelectorAll(".char-button").forEach(item => {
 
     let clazz = item.value
     let ribbon = document.querySelector(`[data-character-class="${clazz}"] .starting-stats`)
+    let classRibbon =  document.querySelector(`[data-character-class="${clazz}"]`)
 
     // ribbon.style.transform = scale(1.5)
     // ribbon.style.display = 'flex';
     if (ribbon.style.display === 'none') {
-            ribbon.style.display = 'flex';
+        ribbon.style.display = 'flex';
+        classRibbon.style.borderColor = 'white'
     } else {  
-        ribbon.style.display = 'none'      
+        ribbon.style.display = 'none' 
+        classRibbon.style.borderColor = 'black'     
     }
 
     })
@@ -111,40 +100,6 @@ document.querySelectorAll(".char-button").forEach(item => {
 
 
 })
-
-// function myFunction() {
-//     var x = document.getElementById('myDIV');
-//     if (x.style.display === 'none') {
-//       x.style.display = 'block';
-//     } else {
-//       x.style.display = 'none';
-//     }
-//   }
-
-
-
-
-
-//what happens (arrow function) when there is a hover?
-      //get the json of the class we are hovering over
-      //item - has a character class item.dataset.characterClass 
-      //characterClass is used for API call 
-      // then use the find the children of that itme needed 
-      //how am I getting the hit die humber into the h3?
-
-
-// if function ot get the card to show upon how big the card gets.
-
-// function showCard(ribbon) {
-// if (ribbon?.width = 1) { console.log("too small") 
-// return 
-// }
-// if (ribbon.width >= 2) {
-//     pcCard.display = 
-//     console.log('success')
-// } else {
-//     null
-// }
 
 
 //Dice Rolling Functions 
@@ -198,72 +153,3 @@ function dicePool(obj){
         // return results or object
 }
 
-
-
-
-
-//I wnat to code that when I hover over a conatier, it reveals its content.
- // data 
-
-
-
-// function characterCreation(obj)
-
-
-//make an event listner that will have the contents of a specific clases cards contents dispalay, so that it goes to the top and does not push down the bottome of the frame
-// would like another animation on hover as well - increased z index, so that it is even higher up, and a darker foreground iamges apeas behind it?
-
-
-// // Party Number buttons
-// const memberOne = document.querySelector("#ch-one")
-// const memberTwo = document.querySelector("#ch-two")
-// const memberThree = document.querySelector("#ch-three")
-// const memberFour = document.querySelector("#ch-four")
-// const memberFive = document.querySelector("#ch-five")
-// // 5e class buttons
-// const barbarian = document.querySelector("#barbarian-button")
-// const bard = document.querySelector("#bard-button")
-// const cleric = document.querySelector("#cleric-button")
-// const druid = document.querySelector("#druid-button")
-// const fighter = document.querySelector("#fighter-button")
-// const monk = document.querySelector("#monk-button")
-// const paladin = document.querySelector("#paladin-button")
-// const ranger = document.querySelector("#ranger-button")
-// const rogue = document.querySelector("#rogue-button")
-// const sorcerer = document.querySelector("#sorcerer-button")
-// const warlock = document.querySelector("#warlock-button")
-// const wizard = document.querySelector("#wizard-button")
-
-
-// barbarian.addEventListener('click', rollChar){
-//     function rollChar() {
-
-//     }
-// }
-
-
-
-
-// CLASS CODE
-
-// const submitButton = document.querySelector('#fetch')
-// const breedInput = document.querySelector('input')
-// const imageDiv = document.querySelector('div')
-// const getBreeds = async() => {
-
-//     const dogBreeds = await axios.get('https://dog.ceo/api/breeds/list/all')
-
-//     console.log(dogBreeds)
-// }
-
-// getBreeds()
-
-
-// submitButton.addEventListener('click', async () => {
-//     let breed = breedInput.value
-//     let response = await axios.get(`https://dog.ceo/api/breed/${breed}/images/random)`)
-// console.log(response.data.message)
-// let dogPic = response.data.message
-// imageDiv.innerHTML = `<img src=${dogPic}>`
-
-//
